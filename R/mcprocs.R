4177b74bcfd7e3eaa18ea644eb8abcda81ae93ad
Fabian Krüger <Fabian.Krueger83@gmail.com>
2014-09-15 13:52:22 +0200

    Initial commit

---
+++ R/mcprocs.R
@@ -0,0 +1,92 @@
+# Numerical integration for CRPS
+crps.int <- function(F, y){
+  s1 <- integrate(function(s) F(s)^2, -Inf, y)$value
+  s2 <- integrate(function(s) (1-F(s))^2, y, Inf)$value
+  s1 + s2
+}
+
+# CRPS of a (weighted) empirical distribution
+crps.edf <- function(dat, y, w = NULL){
+  
+  ord <- order(dat)
+  x <- dat[ord]
+  n <- length(x)
+  
+  # Set uniform weights unless specified otherwise
+  if (is.null(w)){
+    p <- (1:(n-1))/n
+  } 
+  if (!is.null(w)) {
+    if (length(w) != length(dat)) stop()
+    w <- w[ord]
+    p <- cumsum(w[1:(length(w)-1)])
+  }
+  
+  # Compute score
+  if (y < x[1]){
+    
+    a <- x[2:n] - x[1:(n-1)]
+    out <- sum(a*(p^2)) + x[1] - y
+    
+  } else if (y >= x[1] & y <= x[n]){
+    
+    ind <- min(which(x > y)) - 2
+    a <- b <- rep(0, n-1)
+    if (ind > 0){
+      a[1:ind] <- x[2:(ind + 1)] - x[1:ind]
+    }
+    a[ind+1] <- y - x[ind+1]
+    b[ind+1] <- x[ind+2] - y
+    if (ind < (n-2)){
+      b[(ind+2):(n-1)] <- x[(ind+3):(n)] - x[(ind+2):(n-1)]
+    }
+    out <- sum( a*(p^2) + b*((1-p)^2) )
+    
+  } else if (y > x[n]) {
+    
+    b <- x[2:n] - x[1:(n-1)]
+    out <- sum(b*((1-p)^2)) + y - x[n]
+    
+  }
+  
+  return(out)
+  
+}
+
+# Estimate the CRPS of a sample of data (based on kernel representation)
+crps.krep <- function(dat, y){
+  
+  n <- length(dat)
+  shift <- as.integer((n-1)/2)
+  shift <- c((shift+1):n, 1:shift)
+  crps <- mean(abs(y-dat)) - 0.5*mean(abs(dat- dat[shift]))
+  return(crps)
+  
+}
+
+# CRPS for a mixture of normals
+crps.mixn <- function(m, v, y, w = NULL){
+  n <- length(m)
+  if (is.null(w)) w <- rep(1/n, n)
+  -crpsmixnC(w, m, sqrt(v), y)
+}
+
+# CRPS for a t distribution
+crps.t <- function(m, v, df, y){
+  s <- sqrt(v)
+  z <- (y-m)/s
+  c1 <- (2*s*dt(z, df)) * ((df + z^2)/(df - 1)) + z*s*(2*pt(z, df) - 1)
+  c2 <- ( s*(4*sqrt(df))/(df - 1) ) * (beta(0.5, df - 0.5)/(beta(0.5, 0.5*df)^2))
+  c1 - 0.5*c2
+}
+
+# CRPS via kernel density estimation
+crps.kdens <- function(dat, y, bw = NULL){
+  n <- length(dat)
+  if (is.null(bw)){
+    v <- rep(bw.SJ(dat)^2, n)
+  } else {
+    v <- rep(bw^2, n)
+  }
+  return(crps.mixn(dat, v, y))
+}
---
+++ src/mixn.cpp
@@ -0,0 +1,57 @@
+#include <Rcpp.h>
+#include <cmath>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double dnormC(double x){
+	return (1/sqrt(2*M_PI))*exp(-pow(x,2.0)*0.5);
+}
+
+// [[Rcpp::export]]
+double pnormC(double x){
+    // constants
+    double a1 =  0.254829592;
+    double a2 = -0.284496736;
+    double a3 =  1.421413741;
+    double a4 = -1.453152027;
+    double a5 =  1.061405429;
+    double p  =  0.3275911;
+
+    // Save the sign of x
+    int sign = 1;
+    if (x < 0)
+        sign = -1;
+    x = fabs(x)/sqrt(2.0);
+
+    // A&S formula 7.1.26
+    double t = 1.0/(1.0 + p*x);
+    double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
+
+    return 0.5*(1.0 + sign*y);
+}                 
+
+// [[Rcpp::export]]
+double auxcrpsC(double m, double s){
+	return 2*s*dnormC(m/s) + m*(2*pnormC(m/s)-1);
+}
+
+// [[Rcpp::export]]
+double crpsmixnC(NumericVector w, NumericVector m, NumericVector s, double y){
+  int N = m.size();
+  double crps = 0;
+
+  for(int i = 0; i < N; i++) {
+	crps += (-auxcrpsC(y-m[i],s[i])*w[i]);
+    for (int j = 0; j < (i+1); j++){
+      double tmp = 0.5*auxcrpsC(m[i]-m[j],sqrt(pow(s[i],2.0)+pow(s[j],2.0)))*(w[i]*w[j]);
+      crps += tmp;
+      if (i != j){
+        crps += tmp;
+      }
+    }
+  }
+  return crps;
+}
