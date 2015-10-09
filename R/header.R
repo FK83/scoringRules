3229edaa1ac44c9e198922ffe9aafb5c9e7508ff
aijordan <a@jordan-etc.de>
2015-10-09 11:18:13 +0200

    input checks for procedures using samples

--- R/header.R
+++ R/header.R
@@ -1,67 +1,139 @@
 # !!! Important !!! Set orientation = 1 for positive orientation, to - 1 for negative orientation
-# (only used in functions that appear in the package: xx.parametric and xx.sample, where xx = (ls, crps, dss, qs)
+# (only used in functions that appear in the package: xx and xx_sample, where xx = (crps, logs, qs)
 orientation <- -1
 
 ################################################################################
-### xx.sample
+### xx_sample
 
-crps_sample <- function(y, dat, method = "edf", w = NULL, bw = NULL, num_int = FALSE){
-	# Throw error if weights are not equal
-	if (method == "kde" & !is.null(w)){
-		warning("Observation weights not implemented for kernel density estimation - edf used instead.")
-		method <- "edf"
-	}
+crps_sample <- function(y, dat, method = "edf", w = NULL, bw = NULL, num_int = FALSE) {
+    input <- list(y = y, dat = dat)
+    if (!is.null(w)) {
+      input$w <- w
+    }
+    if (!is.null(bw)) {
+      input$bw <- bw
+    }
+    check.crps_sample(input)
+    
+    # Throw error if weights are not equal
+    if (method == "kde" & !is.null(w)) {
+      warning("Observation weights not implemented for kernel density estimation - edf used instead.")
+      method <- "edf"
+    }
+    
+    if (method == "edf") {
+      out <- crps.edf(dat = dat, y = y, w = w)
+    } else if (method == "kde") {
+      # Bandwidth
+      if (is.null(bw)) {
+        bw <- bw.nrd(dat)
+      }
+      if (num_int == FALSE) {
+        # Exact formula
+        out <- crps.kdens(dat = dat, y = y, bw = bw)
+      } else {
+        # Numerical integration
+        FF <- function(x) {
+          sapply(x, function(zz) mean(pnorm(zz, mean = dat, sd = bw)))
+        }
+        aux1 <- integrate(function(s) FF(s)^2,-Inf, y, rel.tol = 1e-6)$value
+        aux2 <- integrate(function(s) (1 - FF(s))^2, y, Inf, rel.tol = 1e-6)$value
+        out <- -(aux1 + aux2)
+        # Message
+        message("Used numerical integration for CPRS (tolerance = 1e-6).")
+      }
+    } else {
+      stop("Unexpected choice of method - please select either edf or kde")
+    }
+    return(orientation * out)
+  }
 
-	if (method == "edf"){
-		out <- crps.edf(dat = dat, y = y, w = w)
-	} else if (method == "kde") {
-		# Bandwidth
-		if (is.null(bw)) bw <- bw.nrd(dat)
-		if (num_int == FALSE){
-		  # Exact formula
-		  out <- crps.kdens(dat = dat, y = y, bw = bw)
-		} else {
-		  # Numerical integration
-		  FF <- function(x){
-			sapply(x, function(zz) mean(pnorm(zz, mean = dat, sd = bw)))
-		  }
-		  aux1 <- integrate(function(s) FF(s)^2, -Inf, y, rel.tol = 1e-6)$value
-		  aux2 <- integrate(function(s) (1-FF(s))^2, y, Inf, rel.tol = 1e-6)$value
-		  out <- -(aux1 + aux2)
-		  # Message
-          message("Used numerical integration for CPRS (tolerance = 1e-6).")
-		}
-	} else {
-		stop("Unexpected choice of method - please select either edf or kde")
-	}
-	return(orientation*out)
-}
-
-qs_sample <- function(y, dat, bw = NULL, num_int = FALSE){
-  if (is.null(bw)) bw <- bw.nrd(dat)
-  if (num_int == FALSE){
-    out <- qsmixnC(w = rep(1/length(dat), length(dat)), m = dat, s = rep(bw, length(dat)), y = y)
+qs_sample <- function(y, dat, bw = NULL, num_int = FALSE) {
+  input <- list(y = y, dat = dat)
+  if (!is.null(bw)) {
+    input$bw <- bw
+  }
+  check.sample(input)
+  
+  if (is.null(bw)) {
+    bw <- bw.nrd(dat)
+  }
+  if (num_int == FALSE) {
+    w <- rep(1 / length(dat), length(dat))
+    out <- qsmixnC(w = w, m = dat, s = rep(bw, length(dat)), y = y)
   } else {
-    ff <- function(x){
+    ff <- function(x) {
       sapply(x, function(zz) mean(dnorm(zz, mean = dat, sd = bw)))
     }
     message("Used numerical integration for qs computation (tolerance = 1e-6).")
     out <- 2*ff(y) - integrate(function(x) ff(x)^2, -Inf, Inf, rel.tol = 1e-6)$value
   }
-  return(orientation*out)
+  return(orientation * out)
 }
 
-logs_sample <- function(y, dat, bw = NULL){
-  message("Using the log score with kernel density estimation tends to be fragile -- see KLTG (2015) for details.")
+logs_sample <- function(y, dat, bw = NULL) {
+  input <- list(y = y, dat = dat)
+  if (!is.null(bw)) {
+    input$bw <- bw
+  }
+  check.sample(input)
+  
+  message(
+    "Using the log score with kernel density estimation tends to be fragile -- see KLTG (2015) for details."
+  )
   if (is.null(bw)) bw <- bw.nrd(dat)
-  out <- lsmixnC(w = rep(1/length(dat), length(dat)), m = dat, s = rep(bw, length(dat)), y = y)
-  return(orientation*out)
+  w <- rep(1 / length(dat), length(dat))
+  s <- rep(bw, length(dat))
+  out <- lsmixnC(w = w, m = dat, s = s, y = y)
+  return(orientation * out)
 }
+
+# input checks for sample functions
+check.sample <- function(input) {
+  
+  input_isnumeric <- sapply(input, is.numeric)
+  if (!all(input_isnumeric)) {
+    stop(paste("Non-numeric input:", paste(names(input)[!input_isnumeric], collapse=", ")))
+  }
+  
+  input_isvector <- sapply(input, is.vector)
+  if (!all(input_isvector)) {
+    stop(paste("Non-scalar or non-vectorial input:", paste(names(input)[!input_isvector], collapse=", ")))
+  }
+  
+  input_lengths <- sapply(input, length)
+  max_length <- max(input_lengths)
+  ref_length <- do.call(c, list(y = 1, dat = max_length, w = max_length, bw = 1))
+  ref_length2 <- do.call(c, list(y = "1", dat = "n", w = "n", bw = "1"))
+  length_diffs <- input_lengths - ref_length[names(input)]
+  if (any(length_diffs != 0)) {
+    stop(paste("Incompatible input vector lengths.",
+               sprintf("Lengths of (%s) should be (%s).",
+                       paste(names(input), collapse = ", "),
+                       paste(ref_length2[names(input)], collapse = ", ")
+               ),
+               sprintf("Given lengths: %s", paste(input_lengths, collapse = ", ")),
+               sep = "\n")
+    )
+  }
+  
+  if (!is.null(w)) {
+    if (any(w < 0 | w > 1)) {
+      stop("Parameter 'w' contains values not in [0, 1].")
+    }
+    if (all.equal(apply(w, 1, sum), rep(1, dim(w)[1])) != TRUE) {
+      stop("Parameter 'w' contains weighting schemes which do not sum up to 1.")
+    }
+  }
+  if (!is.null(bw) & bw < 0)
+    stop("Bandwidth parameter 'bw' is negative.")
+}
+
 
 ################################################################################
 ### parametric
 
-crps <- function(y, family, ...){
+crps <- function(y, family, ...) {
   ind <- c(match(family, names(list_crpsFunctions)),
            match(paste("crps.", family, sep=""), list_crpsFunctions)
   )
@@ -69,9 +141,9 @@ crps <- function(y, family, ...){
   ind <- unique(ind)
   ind <- names(list_crpsFunctions)[ind]
   if (length(ind) > 1) {
-    stop("Ambiguous choice of parametric family - see details section of ?crps.parametric for a list of available choices.")
+    stop("Ambiguous choice of parametric family - see details section of ?crps for a list of available choices.")
   } else if (length(ind) == 0) {
-    stop("Could not find parametric family - see details section of ?crps.parametric for a list of available choices.")  
+    stop("Could not find parametric family - see details section of ?crps for a list of available choices.")  
   }
   
   input <- list(y = y, ...)
@@ -85,7 +157,7 @@ crps <- function(y, family, ...){
   return(orientation*out)
 }
 
-qs <- function(y, family, ...){
+qs <- function(y, family, ...) {
   ind <- c(match(family, names(list_qsFunctions)),
            match(paste("qs.", family, sep=""), list_qsFunctions)
   )
@@ -93,9 +165,9 @@ qs <- function(y, family, ...){
   ind <- unique(ind)
   ind <- names(list_qsFunctions)[ind]
   if (length(ind) > 1) {
-    stop("Ambiguous choice of parametric family - see details section of ?qs.parametric for a list of available choices.")
+    stop("Ambiguous choice of parametric family - see details section of ?qs for a list of available choices.")
   } else if (length(ind) == 0) {
-    stop("Could not find parametric family - see details section of ?qs.parametric for a list of available choices.")  
+    stop("Could not find parametric family - see details section of ?qs for a list of available choices.")  
   }
   
   input <- list(y = y, ...)
@@ -109,7 +181,7 @@ qs <- function(y, family, ...){
   return(orientation*out)
 }
 
-logs <- function(y, family, ...){
+logs <- function(y, family, ...) {
   ind <- c(match(family, names(list_lsFunctions)),
            match(paste("ls.", family, sep=""), list_lsFunctions)
   )
@@ -117,9 +189,9 @@ logs <- function(y, family, ...){
   ind <- unique(ind)
   ind <- names(list_lsFunctions)[ind]
   if (length(ind) > 1) {
-    stop("Ambiguous choice of parametric family - see details section of ?ls.parametric for a list of available choices.")
+    stop("Ambiguous choice of parametric family - see details section of ?logs for a list of available choices.")
   } else if (length(ind) == 0) {
-    stop("Could not find parametric family - see details section of ?ls.parametric for a list of available choices.")  
+    stop("Could not find parametric family - see details section of ?logs for a list of available choices.")  
   }
   
   input <- list(y = y, ...)
