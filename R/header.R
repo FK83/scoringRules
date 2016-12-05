f0199ce9d4f9ee61b8358efcc02568fceabf7259
Fabian Krüger <Fabian.Krueger83@gmail.com>
2016-12-05 14:45:17 +0100

    export flogis

--- R/header.R
+++ R/header.R
@@ -2,7 +2,7 @@
 # (only used in functions that appear in the package: xx and xx_sample, where xx = (crps, logs, qs)
 orientation <- -1
 
-#' @export crps logs crps_sample logs_sample flapl fnorm f2pexp fmixnorm f2pnorm ft fllapl fllogis fexp fgev fgpd
+#' @export crps logs crps_sample logs_sample flapl fnorm f2pexp fmixnorm f2pnorm ft fllapl flogis fllogis fexp fgev fgpd
 #' @importFrom Rcpp evalCpp
 #' @importFrom methods existsFunction
 #' @importFrom stats bw.nrd dbeta dexp dgamma dlnorm dlogis dnbinom dnorm dpois dt dunif integrate
