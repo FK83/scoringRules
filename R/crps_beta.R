afe565105c1191239c1215d3fe931ac355124f6e
aijordan <a@jordan-etc.de>
2017-07-31 16:55:52 +0200

    exporting crps function for beta distribution

---
+++ R/crps_beta.R
@@ -0,0 +1,35 @@
+#' Calculating the CRPS for the beta distribution
+#'
+#' @param y vector of observations.
+#' @param shape1,shape2 vectors of positive shape parameters.
+#' @param lower,upper vectors of lower and upper limits of the distribution. Must be finite.
+#' @return A vector of CRPS values.
+#' @export
+crps_beta <- function(y, shape1, shape2, lower = 0, upper = 1) {
+  if (identical(lower, 0) && identical(upper, 1)) {
+    z <- y
+    z[z < 0] <- 0
+    z[z > 1] <- 1
+    c1 <- y * (2*pbeta(y, shape1, shape2) - 1)
+    c2 <- shape1 / (shape1 + shape2)
+    c3 <- 1 - 2 * pbeta(y, shape1 + 1, shape2)
+    c4 <- 2 / shape1 * beta(2 * shape1, 2 * shape2) / beta(shape1, shape2)^2
+    if (any(ind <- !is.finite(c4))) {
+      c4[ind] <- sqrt(shape2 / (pi * shape1 * (shape1 + shape2)))[ind]  # stirling's approximation
+    }
+    c1 + c2 * (c3 - c4)
+  } else {
+    lower[!is.finite(lower)] <- NaN
+    upper[!is.finite(upper)] <- NaN
+    scale <- upper - lower
+    scale[scale < 0] <- NaN
+    if (all(scale > 0, na.rm = TRUE)) {
+      scale * crps_beta((y - lower) / scale, shape1, shape2)
+    } else {
+      out <- scale * crps_beta((y - lower) / scale, shape1, shape2)
+      ind <- scale == 0
+      out[ind] <- rep_len(abs(y - lower), length(out))[ind]
+      out
+    }
+  }
+}
