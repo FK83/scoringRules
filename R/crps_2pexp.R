42a951c8790769944fc6f205b46aff9f7dddf4ab
aijordan <a@jordan-etc.de>
2017-07-31 16:51:13 +0200

    exporting crps function for two-piece-exponential distribution

---
+++ R/crps_2pexp.R
@@ -0,0 +1,17 @@
+#' Calculating the CRPS for the two-piece-exponential distribution
+#'
+#' @param y vector of observations.
+#' @param scale1,scale2 vectors of positive scale parameters.
+#' @param location vector of location parameters.
+#' @return A vector of CRPS values.
+#' @export
+crps_2pexp <- function(y, scale1, scale2, location = 0) {
+  if (!identical(location, 0)) y <- y - location
+  z <- y
+  z[z < 0] <- 0
+  y[y > 0] <- 0
+  s <- scale1 + scale2
+  s[s == 0] <- Inf
+  crps_expM(-y, scale = scale1, mass = scale2 / s) +
+    crps_expM(z, scale = scale2, mass = scale1 / s)
+}
