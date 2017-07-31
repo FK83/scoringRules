b3883429f4511161cec019dd1ac0e0050d96430c
aijordan <a@jordan-etc.de>
2017-07-31 17:07:07 +0200

    exporting crps function for negative binomial distribution

---
+++ R/crps_nbinom.R
@@ -0,0 +1,18 @@
+#' Calculating the CRPS for the negative binomial distribution
+#'
+#' @param y vector of observations.
+#' @inheritParams stats::pnbinom
+#' @return A vector of CRPS values.
+#' @export
+crps_nbinom <- function(y, size, prob) {
+  if (!requireNamespace("hypergeo", quietly = TRUE)) {
+    stop(paste(
+      "Calculations require an implementation of the gaussian hypergeometric function.",
+      "Please install the following package: hypergeo (>= 1.0)",
+      sep = "\n"))
+  }
+  c1 <- y * (2 * pnbinom(y, size, prob) - 1)
+  c2 <- (1 - prob) / prob ^ 2
+  c3 <- prob * (2 * pnbinom(y - 1, size + 1, prob) - 1) + Re(hypergeo::hypergeo(size + 1, 0.5, 2,-4 * c2))
+  return(c1 - size * c2 * c3)
+}
