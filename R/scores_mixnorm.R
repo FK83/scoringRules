ce426cf0b39810958f3ff9a908e2dcffdb651d56
aijordan <a@jordan-etc.de>
2017-08-01 18:53:34 +0200

    scores_mixnorm logs update

--- R/scores_mixnorm.R
+++ R/scores_mixnorm.R
@@ -1,4 +1,4 @@
-#' Calculating the CRPS for a mixture of normal distribution.
+#' Calculating scores for a mixture of normal distributions.
 #'
 #' @param y vector of observations.
 #' @param m matrix of mean parameters.
@@ -8,13 +8,25 @@
 #'  numerical integration is used.
 #' @param rel_tol only used if \code{exact} is \code{FALSE}; relative tolerance
 #'  for numerical integration.
-#' @return A vector of CRPS values.
+#' @return A vector of score values.
+#' @name scores_mixnorm
+NULL
+
+#' @rdname scores_mixnorm
 #' @export
 crps_mixnorm = function(y, m, s, w, exact = TRUE, rel_tol = 1e-6){
   if (exact == TRUE){
-    out <- sapply(seq_along(y), function(i) crpsmixnC(w[i, ], m[i, ], s[i, ], y[i]))
+    sapply(seq_along(y),
+           function(i) crpsmixnC(w[i, ], m[i, ], s[i, ], y[i]))
   } else {
-    out <- sapply(seq_along(y), function(i) crps.mixnorm.int(y[i], m[i, ], s[i, ], w[i, ], rel_tol))
+    sapply(seq_along(y),
+           function(i) crps.mixnorm.int(y[i], m[i, ], s[i, ], w[i, ], rel_tol))
   }
-  return(out)
 }
+
+#' @rdname scores_mixnorm
+#' @export
+logs_mixnorm <- function(y, m, s, w) {
+  sapply(seq_along(y), function(i) lsmixnC(w[i, ], m[i, ], s[i, ], y[i]))
+}
+  
