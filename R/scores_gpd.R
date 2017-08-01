126d1ebdab8dea4fd0ecf2b3d34b5d5c553eba3b
aijordan <a@jordan-etc.de>
2017-08-01 18:44:08 +0200

    scores_gpd logs update

--- R/scores_gpd.R
+++ R/scores_gpd.R
@@ -1,11 +1,15 @@
-#' Calculating the CRPS for the generalized Pareto distribution
+#' Calculating scores for the generalized Pareto distribution
 #'
 #' @param y vector of observations.
 #' @param shape vector of positive shape parameters.
 #' @param location vector of location parameters.
 #' @param scale vector of positive scale parameters.
 #' @param mass vector of point masses in \code{location}.
-#' @return A vector of CRPS values.
+#' @return A vector of score values.
+#' @name scores_gpd
+NULL
+
+#' @rdname scores_gpd
 #' @export
 # generalized pareto distribution
 crps_gpd <- function(y, shape, location = 0, scale = 1, mass = 0) {
@@ -31,4 +35,10 @@ crps_gpd <- function(y, shape, location = 0, scale = 1, mass = 0) {
   b <- 1 - shape
   
   abs(y) - scale * a * (2 / b * (1 - x^b) - a / (2 - shape))
+}
+
+#' @rdname scores_gpd
+#' @export
+logs_gpd <- function(y, shape, location = 0, scale = 1) {
+  -fgpd(y, location, scale, shape, 0, log = TRUE)
 }
