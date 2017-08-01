4e181d2aed29a33044f6e779c3e9e74bbd5c5d8e
aijordan <a@jordan-etc.de>
2017-08-01 16:11:44 +0200

    scores_2pnorm logs update

--- R/scores_2pnorm.R
+++ R/scores_2pnorm.R
@@ -1,9 +1,13 @@
-#' Calculating the CRPS for the two-piece-normal distribution
+#' Calculating scores for the two-piece-normal distribution
 #'
 #' @param y vector of observations.
 #' @param scale1,scale2 vectors of positive scale parameters.
 #' @param location vector of location parameters.
-#' @return A vector of CRPS values.
+#' @return A vector of score values.
+#' @name scores_2pnorm
+NULL
+
+#' @rdname scores_2pnorm
 #' @export
 crps_2pnorm <- function(y, scale1, scale2, location = 0) {
   if (!identical(location, 0)) y <- y - location
@@ -15,3 +19,8 @@ crps_2pnorm <- function(y, scale1, scale2, location = 0) {
   crps_gtcnorm(y, scale = scale1, upper = 0, umass = scale2 / s) +
     crps_gtcnorm(z, scale = scale2, lower = 0, lmass = scale1 / s)
 }
+
+#' @rdname scores_2pnorm
+#' @export
+logs_2pnorm <- function(y, scale1, scale2, location = 0)
+  -log(f2pnorm(y, location, scale1, scale2))
