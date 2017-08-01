a6a733357ead19b3792d4397e2efc58766fd7d8a
aijordan <a@jordan-etc.de>
2017-08-01 18:55:07 +0200

    scores_pois logs update

--- R/scores_pois.R
+++ R/scores_pois.R
@@ -1,13 +1,22 @@
-#' Calculating the CRPS for the Poisson distribution
+#' Calculating scores for the Poisson distribution
 #'
 #' @param y vector of observations.
 #' @inheritParams stats::ppois
-#' @return A vector of CRPS values.
+#' @return A vector of score values.
+#' @name scores_pois
+#' @importFrom stats ppois dpois
+NULL
+
+#' @rdname scores_pois
 #' @export
-# poisson
 crps_pois <- function(y, lambda) {
   c1 <- (y - lambda) * (2*ppois(y, lambda) - 1)
   c2 <- 2*dpois(floor(y), lambda) -
     exp(-2*lambda) * (besselI(2*lambda, 0) + besselI(2*lambda, 1))
   return(c1 + lambda*c2)
 }
+
+#' @rdname scores_pois
+#' @export
+logs_pois <- function(y, lambda) 
+  -dpois(y, lambda, log = TRUE)
