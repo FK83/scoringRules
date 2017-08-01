9bab32c06d1144866cfdc188c0af211f786abf1a
aijordan <a@jordan-etc.de>
2017-08-01 18:52:39 +0200

    scores_lnorm logs update

--- R/scores_lnorm.R
+++ R/scores_lnorm.R
@@ -1,11 +1,16 @@
-#' Calculating the CRPS for the log-normal distribution
+#' Calculating scores for the log-normal distribution
 #'
 #' @param y vector of observations.
 #' @param meanlog an alternative way to specify \code{locationlog}.
 #' @param sdlog an alternative way to specify \code{scalelog}.
 #' @param locationlog vector of location parameters on the log scale.
 #' @param scalelog vector of positive scale parameters on the log scale.
-#' @return A vector of CRPS values.
+#' @return A vector of score values.
+#' @name scores_lnorm
+#' @importFrom stats plnorm dlnorm
+NULL
+
+#' @rdname scores_lnorm
 #' @export
 crps_lnorm <- function(y, meanlog = 0, sdlog = 1,
                        locationlog = meanlog, scalelog = sdlog) {
@@ -24,3 +29,23 @@ crps_lnorm <- function(y, meanlog = 0, sdlog = 1,
   c3 <- plnorm(y, meanlog + sdlog^2, sdlog) + pnorm(sdlog / sqrt(2)) - 1
   c1 - c2 * c3
 }
+
+
+#' @rdname scores_lnorm
+#' @export
+logs_lnorm <- function(y, meanlog = 0, sdlog = 1,
+                       locationlog = meanlog, scalelog = sdlog) {
+  if (!missing(meanlog) && !missing(locationlog)) {
+    if (all(abs(meanlog - locationlog) < 1e-15))
+      warning("specify 'meanlog' or 'locationlog' but not both")
+    else stop("specify 'meanlog' or 'locationlog' but not both")
+  }
+  if (!missing(sdlog) && !missing(scalelog)) {
+    if (all(abs(sdlog - scalelog) < 1e-15))
+      warning("specify 'sdlog' or 'scalelog' but not both")
+    else stop("specify 'sdlog' or 'scalelog' but not both")
+  }
+  -dlnorm(y, locationlog, scalelog, log = TRUE)
+}
+  
+
