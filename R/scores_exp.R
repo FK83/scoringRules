73762aba8ef27801d093c2c4357287a5e0266b39
aijordan <a@jordan-etc.de>
2017-08-01 18:41:36 +0200

    scores_exp logs update

--- R/scores_exp.R
+++ R/scores_exp.R
@@ -1,6 +1,6 @@
-#' Calculating the CRPS for the exponential distribution
+#' Calculating scores for the exponential distribution
 #'
-#' Calculating the CRPS for the exponential distribution, and the exponential
+#' Calculating scores (CRPS, logarithmic score) for the exponential distribution, and the exponential
 #' distribution with location-scale transformation and point mass in
 #' \code{location}.
 #'
@@ -9,17 +9,34 @@
 #' @param location vector of location parameters.
 #' @param scale vector of positive scale parameters.
 #' @param mass vector of point masses in \code{location}.
-#' @return A vector of CRPS values.
+#' @return A vector of score values.
+#' @name scores_exp
+#' @importFrom stats pexp dexp
+NULL
+
+#' @rdname scores_exp
 #' @export
 crps_exp <- function(y, rate = 1) {
   abs(y) - (2 * pexp(y, rate) - 0.5) / rate
 }
 
-#' @rdname crps_exp
+#' @rdname scores_exp
 #' @export
 crps_expM <- function(y, location = 0, scale = 1, mass = 0) {
   if (!identical(location, 0)) y <- y - location
   mass[mass < 0 | mass > 1] <- NaN
   a <- 1 - mass
   abs(y) - scale * a * (2 * pexp(y, 1 / scale) - 0.5 * a)
+}
+
+#' @rdname scores_exp
+#' @export
+logs_exp <- function(y, rate = 1) {
+  -dexp(y, rate, log = TRUE)
+}
+
+#' @rdname scores_exp
+#' @export
+logs_expM <- function(y, location = 0, scale = 1) {
+  -fexp(y, location, scale, 0, log = TRUE)
 }
