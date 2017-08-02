030782853ef96c7ae9c0b0102bdb46aa386b8b33
aijordan <a@jordan-etc.de>
2017-08-02 11:13:42 +0200

    scores.numeric update

--- R/header.R
+++ R/header.R
@@ -12,38 +12,6 @@ crps <- function(y, ...) UseMethod("crps")
 
 logs <- function(y, ...) UseMethod("logs")
 
-#' @export
-#' @export crps.numeric
-crps.numeric <- function(y, family, ...) {
-  family <- checkFamily(family, "crps")
-  checkInput <- get(paste0("check.", family))
-  calculateCRPS <- get(paste0("crps.", family))
-  
-  input <- list(y = y, ...)
-  input <- checkInput(input)
-  out <- do.call(calculateCRPS, input)
-  
-  if (any(is.na(out))) {
-    warning("Missing CRPS values. Probably due to numerical instabilities as a result of extreme parameter choices.")
-  } else if (any(out < 0)) {
-    warning("Negative CRPS values. Check parameter combinations and/or contact package maintainer(s).")
-  }
-  
-  return(out)
-}
 
-#' @export
-#' @export logs.numeric
-logs.numeric <- function(y, family, ...) {
-  family <- checkFamily(family, "ls")
-  checkInput <- get(paste0("check.", family))
-  calculateLS <- get(paste0("ls.", family))
-  
-  input <- list(y = y, ...)
-  input <- checkInput(input)
-  out <- do.call(calculateLS, input)
-  
-  return(out)
-}
 
 ################################################################################
