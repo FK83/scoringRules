3121ab14bb3ab58ef97be9959bb39a6174842a06
aijordan <a@jordan-etc.de>
2017-08-02 18:11:37 +0200

    initial unit tests for crps_mixnorm functions

---
+++ tests/testthat/test-crps_mixnorm.R
@@ -0,0 +1,21 @@
+context("CRPS for mixture of normal distributions")
+
+test_that("computed values are correct", {
+  expect_equal(crps_mixnorm(-1,
+                            matrix(-1.4, 1, 1),
+                            matrix(.9, 1, 1),
+                            matrix(1, 1, 1)),
+               crps_norm(-1,-1.4, .9),
+               tolerance = 1e-5)
+  expect_equal(
+    crps_mixnorm(
+      -1,
+      matrix(-1.4, 1, 1),
+      matrix(.9, 1, 1),
+      matrix(1, 1, 1),
+      exact = FALSE,
+      rel_tol = 1e-10
+    ),
+    crps_norm(-1,-1.4, .9)
+  )
+})
