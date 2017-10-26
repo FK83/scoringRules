d6ce3db0e90c16b20062f661c48f37d5da4510e5
aijordan <a@jordan-etc.de>
2017-10-26 19:39:44 +0200

    add dss_gev

--- R/scores_gev.R
+++ R/scores_gev.R
@@ -50,7 +50,23 @@ crps_gev <- function(y, shape, location = 0, scale = 1) {
 logs_gev <- function(y, shape, location = 0, scale = 1) {
   -log(fgev(y, location, scale, shape))
 }
-  
+
+#' @rdname scores_gev
+#' @export
+dss_gev <- function(y, shape, location = 0, scale = 1) {
+  if (!identical(location, 0)) y <- y - location
+  shape[shape >= 0.5] <- NaN
+  scale[scale <= 0] <- NaN
+  is_shape0 <- abs(shape) < 1e-12
+  g1 <- gamma(1 - shape)
+  m1 <- (g1 - 1) / shape
+  m1[is_shape0] <- -digamma(1)
+  m <- m1 * scale
+  v1 <- (gamma(1 - 2 * shape) - g1^2) / shape^2
+  v1[is_shape0] <- pi^2 / 6
+  v <- v1 * scale^2
+  (y - m)^2 / v + log(v)
+}
 
 
 check_crps_gev <- function(input) {
