782d0e99771fcfd0e96d83d71ba0aa9c7cc40175
Fabian <fabian.krueger83@gmail.com>
2017-12-05 18:04:37 +0100

    export S3 methods (roxygen @export  tag next to function itself)

--- R/procs_casestudy.R
+++ R/procs_casestudy.R
@@ -1,10 +1,3 @@
-#' @export ar_ms run_mcstudy run_casestudy 
-#' @method plot casestudy
-#' @method plot mcstudy
-#' @method print casestudy
-#' @method print mcstudy
-#' @method summary casestudy
-#' @method summary mcstudy
 #' @importFrom Rcpp evalCpp
 #' @importFrom MASS mvrnorm
 #' @importFrom graphics axis legend lines mtext plot points segments title
