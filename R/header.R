2b12269d8ece4d74562a10ca0f9a2f91f2bcc7a6
Fabian <fabian.krueger83@gmail.com>
2017-12-05 18:03:13 +0100

    Import knitr (needed for new summary methods)

--- R/header.R
+++ R/header.R
@@ -1,5 +1,6 @@
 #' @export flapl fnorm f2pexp fmixnorm f2pnorm ft fllapl flogis fllogis fexp fgev fgpd
 #' @importFrom Rcpp evalCpp
+#' @importFrom knitr kable
 #' @importFrom stats bw.nrd dbeta dexp dgamma dlnorm dlogis dnbinom dnorm dpois dt dunif integrate
 #' @importFrom stats pbeta pexp pgamma plnorm plogis pnbinom pnorm ppois pt punif
 #' @useDynLib scoringRules, .registration = TRUE
