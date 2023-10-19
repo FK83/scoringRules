0b5816805556361c460541a40a840a355519c1c2
fabian.krueger <fabian.krueger@kit.edu>
2023-10-19 09:49:55 +0200

    add qs_sample and ints_sample

--- R/scores_quantiles.R
+++ R/scores_quantiles.R
@@ -1,9 +1,13 @@
 #' Quantile and interval scores
 #' @param y vector of observations
 #' @param x vector of quantile predictions
+#' @param dat vector or matrix (depending on \code{y}; see details)
+#'  of simulation draws from forecast distribution. 
 #' @param x_lower,x_upper vector of quantile predictions (lower and upper endpoints of prediction intervals)
 #' @param alpha quantile level of interest 
 #' @param target_coverage target (i.e., nominal) coverage level of prediction interval
+#' @param type integer between 1 and 9 that is passed on to stats function \link[stats]{quantile} (specifies algorithm for 
+#' empirical quantile estimation; defaults to 7)
 #' @return A vector of score values. Smaller values indicate better forecasts. Note that 
 #' the interval score refers to the central prediction interval at level \code{target_coverage}.
 #' @references 
@@ -13,24 +17,59 @@
 #' Gneiting, T. and A.E. Raftery (2007):
 #' `Strictly proper scoring rules, prediction and estimation',
 #' Journal of the American Statistical Association 102, 359-378. \doi{10.1198/016214506000001437}
+#' 
+#' @seealso The syntax of \code{\link{qs_sample}} and \code{\link{ints_sample}} is analogous to the functions documented on \code{\link{scores_sample_univ}}.
+#'  
+#' @details 
+#' For a vector \code{y} of length n, \code{dat} should be given as a matrix
+#' with n rows. If \code{y} has length 1, then \code{dat} may be a vector.
+#' @name scores_quantiles
 #' @examples
 #' # Interval score is proportional to sum of two quantile scores
-#' 10*(qs(y = 1, x = qnorm(.1), alpha = .1) + qs(y = 1, x = qnorm(.9), alpha = .9))
-#' ints(y = 1, x_lower = qnorm(.1), x_upper = qnorm(.9), target_coverage = .8)
-#' @name scores_quantiles
+#' target_coverage <- .8
+#' # corresponding quantile levels
+#' alpha_1 <- .5*(1-target_coverage)
+#' alpha_2 <- 1-.5*(1-target_coverage)
+#' # compute intervals score
+#' ints(y = 1, x_lower = qnorm(alpha_1), 
+#' x_upper = qnorm(alpha_2), target_coverage = target_coverage)
+#' # compute sum of quantile scores (scaled by 2/(1-target_coverage))
+#' (2/(1-target_coverage))*(qs(y = 1, x = qnorm(alpha_1), alpha = alpha_1) + 
+#' qs(y = 1, x = qnorm(alpha_2), alpha = alpha_2))
+#' # Compare exact to simulated quantile forecast from standard normal distribution
+#' qs(y = 1, x = qnorm(.1), alpha = .1)
+#' qs_sample(y = 1, dat = rnorm(500), alpha = .1)
 #' @export
 qs <- function(y, x, alpha){
   # input checks
   input <- list(y = y, x = x)
   checkNumeric(input)
   checkVector(input)
+  check_alpha(alpha)
+  # score calculation
+  ((y < x)-alpha)*(x-y)
+}
+
+# function to check provided quantile level
+check_alpha <- function(alpha){
   if (!is.numeric(alpha) || length(alpha) != 1 ){
     stop("alpha must be numeric of length 1")
   } else if (alpha <= 0 || alpha >= 1) {
     stop("alpha must satisfy 0 < alpha < 1")
   }
-  # score calculation
-  ((y < x)-alpha)*(x-y)
+}
+
+# function to check inputs for interval score
+check_ints <- function(check_target_coverage, 
+                       x_lower, x_upper){
+  if (!is.numeric(target_coverage) || length(target_coverage) != 1 ){
+    stop("target_coverage must be numeric of length 1")
+  } else if (target_coverage <= 0 || target_coverage >= 1) {
+    stop("target_coverage must satisfy 0 < target_coverage < 1")
+  }
+  if (any(x_lower > x_upper)){
+    stop("'x_lower' contains values greater than corresponding values in 'x_upper'.")  
+  }  
 }
 
 # interval score
@@ -41,16 +80,71 @@ ints <- function(y, x_lower, x_upper, target_coverage){
   input <- list(y = y, x_lower = x_lower, x_upper = x_upper)
   checkNumeric(input)
   checkVector(input)
-  if (!is.numeric(target_coverage) || length(target_coverage) != 1 ){
-    stop("target_coverage must be numeric of length 1")
-  } else if (target_coverage <= 0 || target_coverage >= 1) {
-    stop("target_coverage must satisfy 0 < target_coverage < 1")
-  }
-  if (any(x_lower > x_upper)){
-    stop("'x_lower' contains values greater than corresponding values in 'x_upper'.")  
-  }
+  check_ints(target_coverage, x_lower, x_upper)
   # get interval score as sum of two quantile scores
   aux1 <- qs(y, x_lower, .5*(1-target_coverage)) 
   aux2 <- qs(y, x_upper, .5*(1+target_coverage)) 
   (2/(1-target_coverage))*(aux1 + aux2)
+}
+
+qs_edf <- function(y, dat, alpha, w = NULL, type = 7) {
+  # compute empirical alpha quantile 
+  q_alpha <- quantile(dat, probs = alpha, names = FALSE, type = type)
+  f <- function(s){
+    qs(y = s, x = q_alpha, alpha = alpha)
+  }
+  sapply(y, f)
+}
+
+#' @rdname scores_quantiles
+#' @export
+qs_sample <- function(y, dat, alpha, w = NULL, 
+                      type = 7, show_messages = TRUE) {
+  input <- list(y = y, dat = dat)
+  if ( (!is.null(w)) && show_messages ){
+    message("Parameter 'w' is currently ignored for qs_sample.")
+    w <- NULL 
+  }
+  if (identical(length(y), 1L) && is.vector(dat)) {
+    check_sample(input)
+    qs_edf(y = y, dat = dat, alpha = alpha, type = type)
+  } else {
+    check_sample2(input)
+    sapply(seq_along(y),
+           function(i) qs_edf(y = y[i], dat = dat[i, ], alpha = alpha, 
+                              type = type))
+  }
+}
+
+ints_edf <- function(y, dat, target_coverage, w = NULL, type = 7) {
+  # compute relevant empirical quantiles
+  l <- c(.5*(1-target_coverage), 1-.5*(1-target_coverage))
+  q_alpha <- quantile(dat, probs = l, names = FALSE, type = type)
+  f <- function(s){
+    ints(y = s, x_lower = q_alpha[1], x_upper = q_alpha[2], 
+         target_coverage = target_coverage)
+  }
+  sapply(y, f)
+}
+
+#' @rdname scores_quantiles
+#' @export
+ints_sample <- function(y, dat, target_coverage, w = NULL, 
+                        type = 7, show_messages = TRUE) {
+  input <- list(y = y, dat = dat)
+  if ( (!is.null(w)) && show_messages ){
+    message("Parameter 'w' is currently ignored for ints_sample.")
+    w <- NULL 
+  }
+  if (identical(length(y), 1L) && is.vector(dat)) {
+    check_sample(input)
+    ints_edf(y = y, dat = dat, target_coverage = target_coverage, 
+             type = type)
+  } else {
+    check_sample2(input)
+    sapply(seq_along(y),
+           function(i) ints_edf(y = y[i], dat = dat[i, ], 
+                                target_coverage = target_coverage, 
+                                type = type))
+  }
 }
---
+++ tests/testthat/test-scores_quantiles.R
@@ -0,0 +1,26 @@
+context("Quantile and interval scores")
+
+test_that("computed values are correct", {
+  expect_equal(qs(y = 1, x = 0, alpha = .1), .1)
+  expect_equal(qs(y = 1, x = 0, alpha = .4), 4*qs(y = 1, x = 0, alpha = .1))
+  expect_equal(qs(y = -1, x = 0, alpha = .1), .9)
+
+  expect_equal(ints(y = 1, x_lower = -1, x_upper = 1, target_coverage = .8), 
+               2)
+  expect_equal(ints(y = 1, x_lower = -1, x_upper = 1, target_coverage = .8), 
+               ints(y = 1, x_lower = -1, x_upper = 1, target_coverage = .5))
+  expect_equal(ints(y = 2, x_lower = -1, x_upper = 1, target_coverage = .8), 
+               12)
+  expect_equal(ints(y = 2, x_lower = -1, x_upper = 1, target_coverage = .5), 
+               6)
+  
+  dat <- rnorm(100)
+  expect_equal(ints_sample(y = 1, dat = dat, target_coverage = .6), 
+               5*(qs_sample(y = 1, dat = dat, alpha = .2) + 
+                    qs_sample(y = 1, dat = dat, alpha = .8)))
+  
+  expect_error(ints_sample(y = 1, dat = dat, target_coverage = 1.1))
+  expect_error(ints_sample(y = 1, dat = dat, target_coverage = .8, 
+                           type = "a"))
+  
+})
